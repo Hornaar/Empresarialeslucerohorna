1b1634c68f237328f4a9251f17c472403279b1c4
Hornaar <144254275+Hornaar@users.noreply.github.com>
2023-09-28 12:19:25 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+const salirads = documen.querySelector(".cerrar-ads");
+const ads1 = documen.querySelector(".ads1");
+
+salirads.addEventListener ("click", () => {
+    ads1.classList.add("hiden");
+})
